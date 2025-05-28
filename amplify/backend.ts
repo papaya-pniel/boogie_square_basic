271b5f830eb4dc97176c3b85ef63a2ba74b7a113
Miya Pniel <miya.pniel@version1.com>
2025-05-28 18:37:42 +0100

    rename

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './backend/auth';
+import { auth } from './auth/resource'; // Notice the new path
 
 export const backend = defineBackend({
   auth,
