dc83257a1e2afd011ec488cec054f1f0a2d9002b
Miya Pniel <miya.pniel@version1.com>
2025-05-28 21:07:47 +0100

    add login

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import AppRouter from "./router/AppRouter.jsx";
 import { VideoProvider } from "./context/VideoContext";
 import "./index.css";
+
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 Amplify.configure(outputs);
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,24 @@
-// src/client/router/AppRouter.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-import Login from "../pages/Login.jsx";
 import MainGrid from "../pages/MainGrid.jsx";
 import TrainPage from "../pages/TrainPage.jsx";
 import RecordPage from "../pages/RecordPage.jsx";
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<MainGrid />} />
-        <Route path="/train/:index" element={<TrainPage />} />
-        <Route path="/record/:index" element={<RecordPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainGrid user={user} signOut={signOut} />} />
+            <Route path="/train/:index" element={<TrainPage user={user} />} />
+            <Route path="/record/:index" element={<RecordPage user={user} />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </Authenticator>
   );
 }
