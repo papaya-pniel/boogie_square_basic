9770373f2cbfaa4e6890c81a27076ea37e69e563
Miya Pniel <miya.pniel@version1.com>
2025-07-07 15:18:15 +0100

    debug

--- src/context/VideoContext.jsx
+++ src/context/VideoContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
-import { Auth } from "@aws-amplify/auth";
-import { GraphQLAPI, graphqlOperation } from "@aws-amplify/api-graphql";
 import { uploadData, downloadData, remove } from "aws-amplify/storage";
+import { GraphQLAPI, graphqlOperation } from "@aws-amplify/api-graphql";
+import { Auth } from "aws-amplify/auth";
 
 const Storage = {
   async put(filename, blob, options) {
