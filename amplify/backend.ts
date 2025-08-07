fc23941279cb821a9441912920ea523de96279f0
Miya Pniel <miya.pniel@version1.com>
2025-08-07 13:39:53 +0100

    debug

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { addCustomDomain } from './custom-domain/resource'; // Updated name
 
 export const backend = defineBackend({
   auth,
   data,
 });
-
-const customStack = backend.createStack('CustomDomainStack');
-addCustomDomain(customStack); // Call the function with the stack
